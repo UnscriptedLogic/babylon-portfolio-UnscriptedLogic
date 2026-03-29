6f8b6d9df046f3f4d0138f70b07c390e786c6d94
Nathaniel Alcala <86519190+UnscriptedLogic@users.noreply.github.com>
2026-03-29 23:19:37 +0800

    Update UtilityFunctions.ts

--- src/utility/UtilityFunctions.ts
+++ src/utility/UtilityFunctions.ts
@@ -22,7 +22,10 @@ import { Interpolation, Tween } from "@tweenjs/tween.js";
 import { Fonts } from "../manager/fontmanager";
 
 export const ImportCustomModel = async (fileName: string, scene: Scene) => {
-    const result = ImportMeshAsync(`/models/Mesh_${fileName}.glb`, scene);
+    const result = ImportMeshAsync(
+        `../public/models/Mesh_${fileName}.glb`,
+        scene,
+    );
     return result;
 };
 
